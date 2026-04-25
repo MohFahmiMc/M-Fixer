df59c626f0182c9a8ebfbdbf6c2897ab3ca10890
MohFahmiMc <mohf57880@gmail.com>
2026-04-25 21:33:28 +0700

    Update MainActivity.kt

--- app/src/main/java/com/scarilyid/mfixer/MainActivity.kt
+++ app/src/main/java/com/scarilyid/mfixer/MainActivity.kt
@@ -1,11 +1,25 @@
-.github/workflows/build.yml
-app/
-  build.gradle
-  src/
-    main/
-      java/com/scarilyid/mfixer/MainActivity.kt
-      res/ (isi logo dan layout di sini)
-      AndroidManifest.xml
-build.gradle
-settings.gradle
-gradlew
+package com.scarilyid.mfixer
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnAccess = findViewById<Button>(R.id.btnAccess)
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+
+        btnAccess.setOnClickListener {
+            Toast.makeText(this, "Akses Minecraft diberikan!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this, "Fitur Setting segera hadir", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
