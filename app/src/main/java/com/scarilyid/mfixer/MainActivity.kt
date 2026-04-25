d2aa0166123028ee0c885c822813d50373070489
MohFahmiMc <mohf57880@gmail.com>
2026-04-25 20:46:53 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/scarilyid/mfixer/MainActivity.kt
@@ -0,0 +1,11 @@
+.github/workflows/build.yml
+app/
+  build.gradle
+  src/
+    main/
+      java/com/scarilyid/mfixer/MainActivity.kt
+      res/ (isi logo dan layout di sini)
+      AndroidManifest.xml
+build.gradle
+settings.gradle
+gradlew
