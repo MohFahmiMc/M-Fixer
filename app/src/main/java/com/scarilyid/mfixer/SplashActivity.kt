500ccf461406ae8535e652d11d803f6707c1fa1a
MohFahmiMc <mohf57880@gmail.com>
2026-04-25 21:34:06 +0700

    Update SplashActivity.kt

--- app/src/main/java/com/scarilyid/mfixer/SplashActivity.kt
+++ app/src/main/java/com/scarilyid/mfixer/SplashActivity.kt
@@ -7,12 +7,11 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Ini akan memanggil layout activity_splash.xml yang kamu buat
         setContentView(R.layout.activity_splash)
 
-        // Loading selama 2 detik sebelum pindah ke MainActivity
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
